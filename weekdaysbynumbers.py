72b01510cd111678b30eff123fd8721655f45273
Kohit Prajapat <70266350+kohitprajapat@users.noreply.github.com>
2023-01-12 00:27:31 +0530

    Add files via upload

---
+++ weekdaysbynumbers.py
@@ -0,0 +1,25 @@
+day = input("Enter a day number: ")
+
+if day == "1" :
+    print("Entered day is Monday")
+    
+elif day == "2" :
+    print("Entered day is Tuesday")
+    
+elif day == "3" :
+    print("Entered day is Wednesday")
+    
+elif day == "4" :
+    print("Entered day is Thursday")
+    
+elif day == "5" :
+    print("Entered day is Friday")
+    
+elif day == "6" :
+    print("Entered day is Saturday")
+    
+elif day == "7" :
+    print("Entered day is Sunday")
+    
+else :
+    print("You entered a number more than 7")
